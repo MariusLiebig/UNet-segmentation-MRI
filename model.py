7e2c29e2f221642a0f852456d0a7b94c7925377b
MariusLiebig <marius.liebig24@gmail.com>
2025-04-28 12:54:03 +0200

    fixed attention

--- model.py
+++ model.py
@@ -46,12 +46,16 @@ class UNETBase(nn.Module):
         g : gating signal (from decoder)
         """
         # Example: skip: (128, 128, 256) and g: (64, 64, 512)
+        print("skip shape: ", skip.shape)
+        print("g shape: ", g.shape)
 
         # reduce shape of skip -> skip: (64, 64, 256)
         theta_skip = self.conv(skip.size(1), skip.size(1), kernel_size=1, stride=2)(skip)
         # reduce g channels -> g:(64, 64, 256)
-        phi_g = self.conv(g.size(1), skip.size(1) // 2, kernel_size=1, stride=1)(g)
+        phi_g = self.conv(g.size(1), g.size(1) // 2, kernel_size=1, stride=1)(g)
         
+        print("theta_skip shape: ", theta_skip.shape)
+        print("phi_g shape: ", phi_g.shape)
         # Sum
         f = torch.relu(theta_skip + phi_g)
 
@@ -62,7 +66,7 @@ class UNETBase(nn.Module):
         psi = torch.sigmoid(psi)
 
         # Upsample (128, 128, 256)
-        up = self.convtranspose(f.size(1), f.size(1), kernel_size = 2, stride = 2)
+        up = self.convtranspose(1, 1,  kernel_size = 2, stride = 2)(psi)
         out = up*skip
         return out
 
@@ -122,20 +126,20 @@ class UNET3D(UNETBase):
         skip_connections = skip_connections[::-1]
 
         for i in range(0, len(self.decoder), 2):
-            x = self.decoder[i](x)
+            x_up = self.decoder[i](x) #Upsample
             skip_connection = skip_connections[i // 2]
-            if x.shape != skip_connection.shape:
-                x = F.interpolate(x, size=skip_connection.shape[2:], mode='trilinear', align_corners=True)
+            if x_up.shape != skip_connection.shape:
+                x_up = F.interpolate(x_up, size=skip_connection.shape[2:], mode='trilinear', align_corners=True)
             
-            attention = self.attention_block(skip_connection, x)
-            x = torch.cat((attention, x), dim=1)
-            x = self.decoder[i + 1](x)
+            attention = self.attention_block(skip_connection, x) #x -> ouput before upsampling
+            x = torch.cat((attention, x_up), dim=1)
+            x = self.decoder[i + 1](x) #Double conv
 
         return self.final_conv(x)
 
 if __name__ == "__main__":
     model = UNET3D(input_channels=1, output_channels=1, feature_size=[64, 128, 256, 512])
-    x = torch.randn((2, 1, 128, 128))  # Example input
+    x = torch.randn((2, 1, 128, 128, 16))  # Example  (B, C, H, W, D)
     y = model(x)
     print("Input shape: ", x.shape)
     print("Output shape: ", y.shape)
