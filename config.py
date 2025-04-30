e128e1a3e019b875f74e94bff8e8d1794a0fc99e
MariusLiebig <marius.liebig24@gmail.com>
2025-04-30 10:40:18 +0200

    minor changes

--- config.py
+++ config.py
@@ -18,6 +18,6 @@ CONFIG = {
 }
 
 
-base_path = "/datasets/tdt4265/mic/open/HNTS-MRG"
+#base_path = "/datasets/tdt4265/mic/open/HNTS-MRG"
 # base_path = "/home/mariusliebig/Documents/DYP/HNTS-MRG"
-# base_path = "/cluster/projects/vc/data/mic/open/HNTS-MRG"
+base_path = "/cluster/projects/vc/data/mic/open/HNTS-MRG"
