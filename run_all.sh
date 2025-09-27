8d10746d32d4f56afa7f68c73ef1ba8a444612cd
MariusLiebig <159023214+MariusLiebig@users.noreply.github.com>
2025-09-27 12:03:02 +0200

    Delete run_all.sh

--- run_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FRACTIONS=( 0.1 0.2 0.3 0.5 0.8 1.0)
-
-for frac in "${FRACTIONS[@]}"
-do
-    echo "Running training with keep_background_fraction=$frac"
-    python main.py --keep_background_fraction $frac
-done
